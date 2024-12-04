14fb79b9408bafe896b43da1f4cd115cac908a6d
Muhammet KILIÇ <klc.muhammet@icloud.com>
2024-12-04 14:55:51 +0300

    CICD Refactor

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -94,8 +94,8 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':(
-        'django.contrib.auth.password_validation.' 
+        'NAME': (
+        'django.contrib.auth.password_validation.',
         'UserAttributeSimilarityValidator'
         ),
     },
